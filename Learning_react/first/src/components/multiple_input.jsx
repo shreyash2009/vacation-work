dc86bdf14327f665431ee14301dcd39acca0334a
shreyashe <shreyash.22010681@viit.ac.in>
2022-06-30 20:45:37 +0530

    multiple input handling files added

---
+++ Learning_react/first/src/components/multiple_input.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState } from 'react';
+import './multiple_input.css';
+
+const MulInput = ()=>{
+    let [fullName, setFullName] = useState({
+        fName : "",
+        lName : ""
+    });
+
+
+
+    const UpdateInfo = (event)=>{
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setFullName((preValue)=>{                                   //all the update part in hook passes a argument with the help of that we can access the previous values before updation
+            if(name === 'fname'){
+                return{
+                    fName : value,
+                    lName : preValue.lName
+                }
+            }
+            else if (name === 'lname'){
+                return{
+                    fName : preValue.fName,
+                    lName : value
+                }
+            }
+        });
+    };
+    const onSubmits = (event)=>{
+        event.preventDefault();
+    };
+
+
+    return(
+        <div className='Container'>
+            <form onSubmit={onSubmits}>
+                <h1>Good Morning, {fullName.fName} {fullName.lName}</h1>
+                <div className='inp'>
+            <input type="text" name='fname' placeholder='Enter Your First Name' onChange={UpdateInfo} value={fullName.fName}/>
+            <input type="text" name= 'lname' placeholder='Enter Your Last Name' onChange={UpdateInfo} value={fullName.lName} />
+            <button type = 'submit' >Show Me </button>
+            </div>
+            </form >
+        </div>
+    );
+
+
+};
+
+
+
+export default MulInput;
--- Learning_react/first/src/index.js
+++ Learning_react/first/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 // import Counter from './components/Counter'
 // import Clock from './components/Clock';
 // import Event  from './components/events';
-import Form from './components/Form'
+// import Form from './components/Form'
+import MulInput  from './components/multiple_input';
 const root =document.getElementById('root');
 
-ReactDOM.render(<Form/>, root);
+ReactDOM.render(< MulInput/>, root);
